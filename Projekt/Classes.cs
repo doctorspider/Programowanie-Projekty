6c6a23075d7d99d7fd8bf682227bd801809e3a3f
doctorspider <spider.36@wp.eu>
2015-06-03 17:18:08 +0200

    v01

---
+++ Projekt/Classes.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt
+{
+    class Users
+    {
+        public string User { get; set; }
+        public string Password { get; set; }
+        public bool IsBanned { get; set; }
+    }
+    interface IConnections
+    {
+        void Connect();
+        void AddUser(string user, string password);
+        void DelUser(string user);
+        List<Users> ListUsers();
+        void ApplyBans(List<Users> users);
+    }
+
+    class Ado : IConnections
+    {
+
+        private string connectionString { get; set; }
+
+        SqlConnection con;
+        string cmd;
+        SqlCommand com;
+
+        public Ado(string ConnectionString)
+        {
+            this.connectionString = ConnectionString;
+        }
+        public void Connect()
+        {
+            con = new SqlConnection(connectionString);
+            con.Open();
+        }
+
+        public void AddUser(string user, string password)
+        {
+            cmd = "INSERT INTO Users([user], [password]) VALUES (@user, @pass)";
+            com = new SqlCommand(cmd, con);
+            com.Parameters.AddWithValue("@user", user);
+            com.Parameters.AddWithValue("@pass", password);
+
+            com.ExecuteNonQuery();
+        }
+
+        public void DelUser(string user)
+        {
+            cmd = "DELETE FROM Users WHERE [user] = @user";
+            com = new SqlCommand(cmd, con);
+            com.Parameters.AddWithValue("@user", user);
+
+            com.ExecuteNonQuery();
+        }
+
+
+        public List<Users> ListUsers()
+        {    
+            cmd = "SELECT [User], [password], isBanned FROM Users";
+            com = new SqlCommand(cmd, con);
+
+            List<Users> tmp = new List<Users>();
+
+            using (SqlDataReader sr = com.ExecuteReader())
+            {
+                while (sr.Read())
+                {
+                    tmp.Add(new Users() { User = (string)sr[0], Password = (string)sr[1], IsBanned = (bool)sr[2] });                  
+                }
+            }
+
+            return tmp;            
+            
+        }
+
+        public void ApplyBans(List<Users> users)
+        {
+            foreach (var user in users)
+            {
+                cmd = "UPDATE Users SET isBanned=@ban WHERE [User]=@user";
+                com = new SqlCommand(cmd, con);
+                com.Parameters.AddWithValue("@ban", user.IsBanned);
+                com.Parameters.AddWithValue("@user", user.User);
+
+                com.ExecuteNonQuery();
+            } 	        
+        }
+}
+
+
+    class LinQ : IConnections
+    {
+        LinQDataDataContext con;
+
+        public void Connect()
+        {
+            con = new LinQDataDataContext();
+        }
+
+        public void AddUser(string user, string password)
+        {
+            UsersTab T = new UsersTab()
+            {
+                user1 = user,
+                password = password,
+                isBanned = false
+            };
+
+            con.UsersTabs.InsertOnSubmit(T);
+
+            con.SubmitChanges();
+        }
+
+        public void DelUser(string user)
+        {
+            var todelete = from x in con.UsersTabs where x.user1 == user select x;
+            foreach (var _user in todelete)
+            {
+                con.UsersTabs.DeleteOnSubmit(_user);
+            }
+
+            con.SubmitChanges();
+        }
+
+        public List<Users> ListUsers()
+        {
+            var users = new List<Users>();
+            foreach (var user in con.UsersTabs)
+            {
+                users.Add(new Users() { User = user.user1, Password = user.password, IsBanned = (bool)user.isBanned });
+            }
+            return users;
+        }
+
+        public void ApplyBans(List<Users> users)
+        {
+            foreach (var user in users)
+            {
+                var changes = from x in con.UsersTabs where x.user1 == user.User select x;
+                foreach (var item in changes)
+                {
+                    item.isBanned = user.IsBanned;
+                }
+
+            }
+
+            con.SubmitChanges();
+        }
+    }
+
+    class Entity : IConnections
+    {
+        ProgramowanieEntities con;
+        public void Connect()
+        {
+            con = new ProgramowanieEntities();
+        }
+
+        public void AddUser(string user, string password)
+        {
+            UsersEntityTab t = new UsersEntityTab()
+            {
+                user = user,
+                password = password,
+                isBanned = false
+            };
+
+            con.Users.Add(t);
+            con.SaveChanges();
+        }
+
+        public void DelUser(string user)
+        {
+            var todelete = from x in con.Users where x.user == user select x;
+            foreach (var _user in todelete)
+            {
+                con.Users.Remove(_user);
+            }
+
+            con.SaveChanges();
+
+        }
+
+        public List<Users> ListUsers()
+        {
+            var users = new List<Users>();
+            foreach (var user in con.Users)
+            {
+                users.Add(new Users() { User = user.user, Password = user.password, IsBanned = (bool)user.isBanned });
+            }
+            return users;
+        }
+
+        public void ApplyBans(List<Users> users)
+        {
+            foreach (var user in users)
+            {
+                var changes = from x in con.Users where x.user == user.User select x;
+                foreach (var item in changes)
+                {
+                    item.isBanned = user.IsBanned;
+                }
+
+                con.SaveChanges();
+            }
+        }
+    }
+
+   
+}
---
+++ Projekt/MainWindow.xaml.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Projekt
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            BanList = new List<Users>();
+        }
+
+        IConnections Connection;
+        string constr = "Data Source=PC;Initial Catalog=Programowanie;Integrated Security=True";
+        List<Users> BanList;
+
+
+        public void Check(object sender=null, RoutedEventArgs e=null)
+        {
+            if ((bool)AdoButton.IsChecked)
+                Connection = new Ado(constr);
+            else if ((bool)LinQButton.IsChecked)
+                Connection = new LinQ();
+            else
+                Connection = new Entity();
+
+            Connection.Connect();
+        }
+
+        private void TextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            var box = (sender as TextBox);
+
+            if ( (box.Text == "User") || (box.Text == "Password") )
+                box.Text = null;
+
+        }
+
+
+        private void User_LostFocus(object sender, RoutedEventArgs e)
+        {
+            var box = (sender as TextBox);
+
+            if (box.Text == "")
+                box.Text = "User";           
+        }
+
+        private void Passwd_LostFocus(object sender, RoutedEventArgs e)
+        {
+            var box = (sender as TextBox);
+
+            if (box.Text == "")
+                box.Text = "Password";  
+        }
+
+        private void ListUsers(object sender, RoutedEventArgs e)
+        {
+            Check();
+
+            OutputList.Items.Clear();
+            foreach (var user in Connection.ListUsers())
+            {
+                var cb = new CheckBox()
+                {
+                    Content = (user.User + " " + user.Password),
+                    IsChecked = user.IsBanned,                  
+                    Foreground = new SolidColorBrush(Color.FromRgb(207, 212, 207))
+                };
+                cb.Click += cb_Click;
+                cb.MouseRightButtonDown += cb_Click;
+                cb.Checked += cb_Checked;
+                cb.Unchecked += cb_Checked;
+
+                OutputList.Items.Add(cb);
+
+                BanList.Clear();
+            }
+        }
+
+        void cb_Checked(object sender, RoutedEventArgs e)
+        {
+            var cb = (sender as CheckBox);
+            BanList.Add(new Users() { User = cb.Content.ToString().Split(' ')[0], IsBanned = (bool)cb.IsChecked });
+            if ((bool)Autoban.IsChecked)
+            {
+                Connection.ApplyBans(BanList);
+                BanList.Clear();
+            }
+        }
+
+        void cb_Click(object sender, RoutedEventArgs e)
+        {
+            var cb = (sender as CheckBox);
+            var tmp = cb.Content.ToString().Split(' ');
+            UserBox.Text = tmp[0];
+            PasswdBox.Text = tmp[1];
+        }
+
+        private void AddUserButton(object sender, RoutedEventArgs e)
+        {
+            Connection.AddUser(UserBox.Text, PasswdBox.Text);
+            ListUsers(sender, null);
+        }
+
+        private void DeleteUserButton(object sender, RoutedEventArgs e)
+        {
+            Connection.DelUser(UserBox.Text);
+            ListUsers(sender, null);
+        }
+
+        private void ApplyBansButton(object sender, RoutedEventArgs e)
+        {
+            Connection.ApplyBans(BanList);
+        }
+
+        private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            
+            Application.Current.MainWindow.DragMove();
+                
+        }
+
+        private void CloseClick(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+    
+
+
+    }
+}
